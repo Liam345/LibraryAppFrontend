6e18f6bb0e98bc99e951f4651b99bf4e01fa821d
Liam Banga <liam205@outlook.com>
2018-08-11 15:54:38 +1000

    Styled the NavBar

--- src/ui/PrimaryHeader.js
+++ src/ui/PrimaryHeader.js
@@ -1,22 +1,56 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
+import styled from "styled-components";
+
+const Nav = styled.div`
+  width: 100%;
+  height: 50px;
+  background-color: #108ee9;
+  display: flex;
+  font-size: 120%;
+  justify-content: flex-start;
+`;
+
+const Logo = styled.div`
+  flex: 3;
+  color: #fff;
+  align-self: center;
+`;
+
+const linkStyle = {
+  flex: "1",
+  color: "#fff",
+  alignSelf: "center"
+};
 
 const PrimaryHeader = ({ user }) => (
-  <header>
-    Welcome to our Bookstore!
-    {user.userData ? user.userData.lastName : ""}
-    <NavLink to="/" exact>
+  <Nav>
+    <Logo>
+      Welcome to our Bookstore!
+      {user.userData ? user.userData.lastName : ""}
+    </Logo>
+    <NavLink to="/" exact style={linkStyle}>
       Home
     </NavLink>
-    <NavLink to="/cart">Cart</NavLink>
-    {user.isAdmin && <NavLink to="/app">App</NavLink>}
-    {user.isLoggedIn ? (
-      <NavLink to="/logout">Logout</NavLink>
-    ) : (
-      <NavLink to="/login">Login</NavLink>
+    <NavLink to="/cart" style={linkStyle}>
+      Cart
+    </NavLink>
+    {user.isAdmin && (
+      <NavLink to="/app" style={linkStyle}>
+        App
+      </NavLink>
     )}
-  </header>
+    {user.isLoggedIn ? (
+      <NavLink to="/logout" style={linkStyle}>
+        Logout
+      </NavLink>
+    ) : (
+      <NavLink to="/login" style={linkStyle}>
+        Login
+      </NavLink>
+    )}
+  </Nav>
 );
 
 function mapStateToProps(state) {
